81a1ebf4e9d3fe43e16c6135dc92f83da4ce040d
StoneStoneStoneWang <yuanxingfu1314@163.com>
2020-04-08 09:09:21 +0800

    EDT

---
+++ Framework/EDTBase/EDTBase.framework/Headers/EDTBase.h
@@ -0,0 +1,20 @@
+//
+//  EDTBase.h
+//  EDTBase
+//
+//  Created by 王磊 on 2020/4/8.
+//  Copyright © 2020 王磊. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EDTBase.
+FOUNDATION_EXPORT double EDTBaseVersionNumber;
+
+//! Project version string for EDTBase.
+FOUNDATION_EXPORT const unsigned char EDTBaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EDTBase/PublicHeader.h>
+
+
+#import <EDTBase/EDTBaseViewController.h>
