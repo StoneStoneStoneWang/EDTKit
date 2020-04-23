765853565ff505c1afe60bc88f51d7660e541a29
StoneStoneStoneWang <yuanxingfu1314@163.com>
2020-04-23 17:53:42 +0800

    EDT

---
+++ Framework/EDTYY/EDTYY.framework/Headers/EDTYY.h
@@ -0,0 +1,20 @@
+//
+//  EDTYY.h
+//  EDTYY
+//
+//  Created by 王磊 on 2020/4/21.
+//  Copyright © 2020 王磊. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EDTYY.
+FOUNDATION_EXPORT double EDTYYVersionNumber;
+
+//! Project version string for EDTYY.
+FOUNDATION_EXPORT const unsigned char EDTYYVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EDTYY/PublicHeader.h>
+
+
+#import <EDTYY/EDTYYCahce.h>
