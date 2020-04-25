2aaf90448e6e2492c0f933f5a42b29fe0e703592
StoneStoneStoneWang <yuanxingfu1314@163.com>
2020-04-25 12:01:08 +0800

    EDT

---
+++ Framework/EDTShare/EDTShare.framework/Headers/EDTShare.h
@@ -0,0 +1,19 @@
+//
+//  EDTShare.h
+//  EDTShare
+//
+//  Created by 王磊 on 2020/4/25.
+//  Copyright © 2020 王磊. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EDTShare.
+FOUNDATION_EXPORT double EDTShareVersionNumber;
+
+//! Project version string for EDTShare.
+FOUNDATION_EXPORT const unsigned char EDTShareVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EDTShare/PublicHeader.h>
+
+#import <EDTShare/EDTShareManager.h>
