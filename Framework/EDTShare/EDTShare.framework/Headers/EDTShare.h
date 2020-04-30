ed4d82b924e474cd72d1a13b8cb778e8ff687282
StoneStoneStoneWang <yuanxingfu1314@163.com>
2020-04-30 10:32:49 +0800

    EDT

--- Framework/EDTShare/EDTShare.framework/Headers/EDTShare.h
+++ Framework/EDTShare/EDTShare.framework/Headers/EDTShare.h
@@ -16,4 +16,20 @@ FOUNDATION_EXPORT const unsigned char EDTShareVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <EDTShare/PublicHeader.h>
 
-#import <EDTShare/EDTShareManager.h>
+@import UIKit;
+
+NS_ASSUME_NONNULL_BEGIN
+NS_SWIFT_NAME(EDTShare)
+@interface EDTShare : NSObject
+
++ (void)EDTShareDownloadWithFrom:(UIViewController * _Nonnull)from
+                      andAppleId:(NSString * _Nonnull)appleId
+                    andShareIcon:(NSString * _Nonnull)shareIcon;
+
+
++ (void)EDTShareWithFrom:(UIViewController * _Nonnull)from
+            andUrlString:(NSString * _Nonnull)urlString
+            andShareIcon:(NSString * _Nonnull)shareIcon;
+@end
+
+NS_ASSUME_NONNULL_END
